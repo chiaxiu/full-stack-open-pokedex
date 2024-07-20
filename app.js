10f3d3a531583ba6bd367b80c110d10ad14b2f6c
chiaxiu <146698230+chiaxiu@users.noreply.github.com>
2024-07-20 19:23:30 +0800

    part11.12 draft

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 // get the port from env variable
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3000;
 
 app.use(express.static('dist'));
 
@@ -12,4 +12,10 @@ app.listen(PORT, () => {
 
 app.get('/version', (req, res) => {
   res.send('1'); // change this string to ensure a new version deployed
+});
+
+app.get('/health', (req, res) => {
+  // eslint-disable-next-line no-constant-condition
+  if (true) throw 'error...  ';
+  res.send('ok');
 });
