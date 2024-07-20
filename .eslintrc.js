1c66cde07f8badc01901819e1acacc6c9efd4132
chiaxiu <146698230+chiaxiu@users.noreply.github.com>
2024-07-20 17:13:37 +0800

    part11.6

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,49 +1,22 @@
 module.exports = {
-  "env": {
-    "browser": true,
-    "es6": true,
-    "jest/globals": true
+  env: {
+    commonjs: true,
+    es2021: true,
+    node: true
   },
-  "extends": [
-    "eslint:recommended",
-    "plugin:react/recommended"
+  overrides: [
+    {
+      env: {
+        node: true
+      },
+      files: ['.eslintrc.{js,cjs}'],
+      parserOptions: {
+        sourceType: 'script'
+      }
+    }
   ],
-  "parserOptions": {
-    "ecmaFeatures": {
-      "jsx": true
-    },
-    "ecmaVersion": 2018,
-    "sourceType": "module"
+  parserOptions: {
+    ecmaVersion: 'latest'
   },
-  "plugins": [
-    "react", "jest"
-  ],
-  "rules": {
-    "indent": [
-      "error",
-        2
-      ],
-    "linebreak-style": [
-      "error",
-      "unix"
-    ],
-    "quotes": [
-      "error",
-      "single"
-    ],
-    "semi": [
-      "error",
-      "never"
-    ],
-    "eqeqeq": "error",
-    "no-trailing-spaces": "error",
-    "object-curly-spacing": [
-      "error", "always"
-    ],
-    "arrow-spacing": [
-      "error", { "before": true, "after": true }
-    ],
-    "no-console": "error",
-    "react/prop-types": 0
-  }
-}
+  rules: {}
+};
---
+++ eslint.config.mjs
@@ -0,0 +1,16 @@
+import globals from 'globals';
+import js from '@eslint/js';
+
+export default [
+  js.configs.recommended,
+  {
+    files: ['**/*.js'],
+    languageOptions: {
+      sourceType: 'module',
+      globals: {
+        ...globals.node
+      },
+      ecmaVersion: 'latest'
+    }
+  }
+];
